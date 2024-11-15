5c16e769e689a2e4176ffd811d645ac72d0ecd0d
이흔오 <gms245@naver.com>
2024-11-15 21:04:32 +0900

    feat(util): 비밀번호 검증 함수 작성
    비밀번호 검증을 위한 함수를 작성했습니다

--- frontend/src/app/utils/function/auth.ts
+++ frontend/src/app/utils/function/auth.ts
@@ -15,8 +15,40 @@ export function validatePassword(password: string) {
   }
 }
 
+export function validatePasswordConfirm(
+  password: string,
+  passwordConfirm: string
+) {
+  if (passwordConfirm.length < 8) {
+    return {
+      state: false,
+      caution: "비밀번호는 8자 이상으로 입력해주세요",
+    };
+  } else {
+    if (password === passwordConfirm) {
+      return {
+        state: true,
+        caution: "",
+      };
+    } else {
+      return {
+        state: false,
+        caution: "비밀번호가 일치하지 않습니다",
+      };
+    }
+  }
+}
+
 export function validateUsername(name: string) {
   if (name.length < 3) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function validateEmailConfirm(password: string) {
+  if (password.length < 8) {
     return false;
   } else {
     return true;

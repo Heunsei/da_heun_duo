fff4885f10e98c1658b40a4e3bb2fdf81c0b8c1c
parkdamin <ferrorist@naver.com>
2024-11-18 19:03:04 +0900

    feat(SecurityConfig): Spring Security PasswordEncoder 적용
    백엔드 서버에 Spring Security를 적용했습니다.
    Argon2 알고리즘을 이용한 PasswordEncoder를 적용했습니다.
    
    Closes #2

---
+++ Back/src/main/java/daheun/akgishark_back/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package daheun.akgishark_back.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        int saltLength = 16;
+        int hashLength = 32;
+        int parallelism = 1;
+        int memory = 65536;
+        int iterations = 4;
+        return new Argon2PasswordEncoder(saltLength, hashLength, parallelism, memory, iterations);
+    }
+}
---
+++ Back/src/main/java/daheun/akgishark_back/service/user/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package daheun.akgishark_back.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public CustomUserDetailsService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
